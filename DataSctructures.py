cfa2fee673a7ad33813b79b3a7405595536312ac
LeanoraVilte <tina.ww.lea@gmail.com>
2022-07-31 15:23:10 +0300

    Data Structures
    Graph1 class

--- DataSctructures.py
+++ DataSctructures.py
@@ -73,4 +73,21 @@ class LinkedList:
             self.size = size
 
     def add_edge(self, orig, dest):
-        if orig > self.size or dest > self.size or orig < 0 or dest < 0
+        if orig > self.size or dest > self.size or orig < 0 or dest < 0:
+            print('Invalid Edge')
+        else:
+            self.adj[orig-1][dest-1] = 1
+            self.adj[dest-1][orig-1] = 1
+
+    def remove_edge(self, orig, dest):
+        if orig > self.size or dest > self.size or orig < 0 or dest < 0:
+            print('Invalid Edge')
+        else:
+            self.adj[orig-1][dest-1] = 0
+            self.adj[dest-1][orig-1] = 0
+
+    def display(self):
+        for row in self.adj:
+            print()
+            for val in row:
+                print('{:4}'.format(val), end="")
