01074fac0bba11169358076780efe71f0b7e4544
LeanoraVilte <tina.ww.lea@gmail.com>
2022-07-31 12:05:58 +0300

    Data Structures
    queue class

--- DataSctructures.py
+++ DataSctructures.py
@@ -13,3 +13,19 @@ class Stack:
 
     def print_stack(self):
         print (self.items)
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def print_queue(self):
+        print (self.items)
